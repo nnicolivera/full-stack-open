e92cb276bdaae648a6f5f1ee147480f4008ef6fc
nnicolivera <nnicolivera@hotmail.com>
2025-12-10 12:45:50 -0300

    exercises up to 2.1 done

---
+++ part2/courseinfo/components/Course.jsx
@@ -0,0 +1,24 @@
+const Header = ({ name }) => (
+    <h1>{name}</h1>
+)
+
+const Part = ({ name, exercises }) => (
+    <p>{name} {exercises}</p>
+)
+
+const Content = ({ parts }) => (
+    <>
+        {parts.map(part =>
+            <Part key={part.id} name={part.name} exercises={part.exercises} />
+        )}
+    </>
+)
+
+const Course = ({ course }) => (
+    <div>
+        <Header name={course.name} />
+        <Content parts={course.parts} />
+    </div>
+)
+
+export default Course
