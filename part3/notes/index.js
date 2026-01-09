715f6319afddc1637282566e325014918bb08c1d
nnicolivera <nnicolivera@hotmail.com>
2026-01-09 17:02:12 -0300

    adding logger and unknown endpoint handler middleware to notes

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.use(express.json())
-
 let notes = [
     {
         id: "1",
@@ -20,6 +18,17 @@ let notes = [
         important: true
     }
 ]
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+app.use(express.json())
+app.use(requestLogger)
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -71,6 +80,12 @@ app.delete('/api/notes/:id', (request, response) => {
     notes = notes.filter(note => note.id !== id)
     response.status(204).end()
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT)
